d9bb8aa7e6ef0eba57dcfb3ce5b89af5d4917b3f
BertVoor <voorjansbert@gmail.com>
2024-10-27 11:41:51 +0100

    Add function to create gameboard.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("script loaded");
+
+function Gameboard() {
+    const board = [];
+    const rows = 3;
+    const columns = 3;
+
+    for(let i = 0; i<rows; i++){
+        board[i] = [];
+        for(let j = 0; j<columns;j++){
+            board[i].push(0);
+        }
+    }
+    console.log(board);
+
+}
+
+function Player() {
+
+}
+
